0ae58de8b3b08e224c8ce1f9fe62a5134089f55f
Carla Montano <cm.emprendiendo@gmail.com>
2025-01-14 16:42:36 -0600

    allowed host and requirements updated

--- a_core/settings.py
+++ a_core/settings.py
@@ -34,14 +34,14 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-render-lkkg.onrender.com']
 
 INTERNAL_IPS = (
     '127.0.0.1',
     'localhost://8000'
 )
 
-CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
+CSRF_TRUSTED_ORIGINS = [ 'https://django-render-lkkg.onrender.com' ]
 
 
 # Application definition
